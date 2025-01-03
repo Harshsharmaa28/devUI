efb45c89f31ca6ae0f7f80a230ff6c24be86661a
Harsh Sharma <harshsharma0828@gmail.com>
2025-01-03 14:35:20 +0530

    Added google site verif

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION_KEY} />
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-gray-900 text-[#FFF] antialiased`}
       >
